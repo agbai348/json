7d588de753f5443613597140815c01333cb0e4bc
Autumn <agbai348@learner.dvusd.org>
2022-03-21 09:31:29 -0700

    tytry

---
+++ jason/jason/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jason
+{
+    class WeatherForecast
+    {
+      
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string Summary { get; set; }
+        }
+    }
+
